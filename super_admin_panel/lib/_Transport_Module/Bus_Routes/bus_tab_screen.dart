39989d32ee480acc3645565bfa24562706a30605
Gopikrishna V <117050479+TianKaiJr@users.noreply.github.com>
2025-03-18 14:26:41 +0530

    Bus Route

--- super_admin_panel/lib/_Transport_Module/Bus_Routes/bus_tab_screen.dart
+++ super_admin_panel/lib/_Transport_Module/Bus_Routes/bus_tab_screen.dart
@@ -1,30 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:super_admin_panel/_Panchayat_Module/widgets/custom_appbar.dart';
 import 'package:super_admin_panel/_Transport_Module/Bus_Routes/bus_route_mgr.dart';
 import 'package:super_admin_panel/_Transport_Module/Bus_Routes/busl_list_screen.dart';
 
-class BusRoutesManager extends StatelessWidget {
+class BusRoutesManager extends StatefulWidget {
   const BusRoutesManager({super.key});
 
   @override
+  _BusRoutesManagerState createState() => _BusRoutesManagerState();
+}
+
+class _BusRoutesManagerState extends State<BusRoutesManager>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Bus Routes Manager'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'Buses'),
-              Tab(text: 'Routes'),
-            ],
+    return Scaffold(
+      appBar: const CustomAppBar(title: "Bus Routes Manager"),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.transparent,
+            child: TabBar(
+              controller: _tabController,
+              labelColor: Colors.blue,
+              unselectedLabelColor: Colors.white,
+              indicatorColor: Colors.blue,
+              tabs: const [
+                Tab(text: "Buses"),
+                Tab(text: "Routes"),
+              ],
+            ),
           ),
-        ),
-        body: const TabBarView(
-          children: [
-            BusListScreen(),
-            RouteListScreen(),
-          ],
-        ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: const [
+                BusListScreen(),
+                RouteListScreen(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
